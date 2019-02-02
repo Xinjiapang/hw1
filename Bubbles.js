e6a5985e19c3bbb1f50f56272530bb787494353c
Xinjiapang <47263250+Xinjiapang@users.noreply.github.com>
2019-02-02 15:23:02 -0800

    Update Bubbles.js

--- Bubbles.js
+++ Bubbles.js
@@ -7,7 +7,7 @@ function setup() {
         var bubble = {
             x: random(width),
             y: random(height),
-            radius: random(5, 150)
+            radius: random(5, 150) // the range of the size of bubbles
         };
         bubbles.push(bubble)
     }
@@ -19,24 +19,24 @@ function mousePressed() {
             y: random(height),
             radius: random(5, 150)
         };
-        bubbles.push(bubble)
+        bubbles.push(bubble) // add bubbles which are random
 }
 
 function draw() {
     background(255);
-    noStroke();
+    noStroke(); // remove stroke of the bubbles
 
     for (var i = 0; i < bubbles.length; i++) {
         var bubble = bubbles[i];
 
         if (dist(mouseX, mouseY, bubble.x, bubble.y) < bubble.radius) {
-            fill(0, 200, 200, 255);
+            fill(0, 200, 200, 255); // change color to green
         } else {
-            fill(255, 0, 100, 100);
+            fill(255, 0, 100, 100); // bubble will be pink without mouse.
         }
 
         ellipse(bubble.x, bubble.y, bubble.radius*2);
-        bubble.x += random(-5, 5);
-        bubble.y += random(-9, 9);
+        bubble.x += random(-5, 5); 
+        bubble.y += random(-9, 9); // bubble will move up and down violently
     };
 }
